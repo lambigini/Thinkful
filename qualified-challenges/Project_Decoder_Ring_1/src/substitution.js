07a429895399331b93a60fc7fa24223ffca5fec0
Harrison <harrisontran@outlook.com>
2021-04-26 20:13:23 -0700

    subtitition function encoding done

--- qualified-challenges/Project_Decoder_Ring_1/src/substitution.js
+++ qualified-challenges/Project_Decoder_Ring_1/src/substitution.js
@@ -2,12 +2,57 @@
 // as the index.html and test files rely on this setup to work properly.
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
+function is_unique(str) {
+  var obj = {};
+  for (let i = 0; i < str.length; ++i) {
+    var ch = str[i];
+    if (obj[ch]) return false;
+    obj[ch] = true;
+  }
+  return true;
+}
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+    if (!alphabet || alphabet.length !== 26 || !is_unique(alphabet) ) return false;
+    // encode
+    // get the index of each input in ascii
+let result = "";
+const inputLowerCase = input.toLowerCase();
+
+    for (let index = 0; index < inputLowerCase.length; index++) {
+      const element = inputLowerCase[index];
+
+      if (element.charCodeAt(0) !== 32 ) {
+        const indexElement = element.charCodeAt(0) - 97 ;
+        console.log(`indexElement: ${indexElement}`);
+        // match the index of to apphabet
+        const match = alphabet.charAt(indexElement);
+        console.log(`match: ${match}`);
+        // add character together
+      result += match;
+      } else {
+        result += " ";
+      }
+      
+
+     
+
+    
+      
+    }
+    
+    return result;
+    
+    console.log(`result: ${result}`);
+    console.log(`inputLowerCase: ${inputLowerCase}`);
+
+
+
+
   }
 
   return {
--- qualified-challenges/Project_Decoder_Ring_1/test/substitution.test.js
+++ qualified-challenges/Project_Decoder_Ring_1/test/substitution.test.js
@@ -1,1 +1,61 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { substitution } = require("../src/substitution");
+
+describe("substitution", () => {
+  it("should return false if the substitution alphabet is missing", () => {
+    const input = "thinkful";
+    const expected = false;
+
+    expect(substitution(input)).to.equal(expected);
+  });
+
+  it("should return false if the substitution alphabet is not exactly 26 characters", () => {
+    const input = "thinkful";
+    const alphabet = "short";
+
+    const actual = substitution(input, alphabet);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  });
+
+  it("should return false if the substitution alphabet does not contain unique characters", () => {
+    const input = "thinkful";
+    const alphabet = "abcabcabcabcabcabcabcabcyz";
+
+    const actual = substitution(input, alphabet);
+    const expected = false;
+    expect(actual).to.equal(expected);
+  });
+
+  it("should encode a message by using the given substitution alphabet", () => {
+    const input = "thinkful";
+    const alphabet = "xoyqmcgrukswaflnthdjpzibev";
+
+    const actual = substitution(input, alphabet);
+    const expected = 'jrufscpw';
+    expect(actual).to.equal(expected);
+  });
+
+  it("should work with any kind of key with unique characters", () => {
+    const input = "message";
+    const alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+
+    const actual = substitution(input, alphabet);
+    const expected = "y&ii$r&";
+    expect(actual).to.equal(expected);
+
+  });
+
+  it("should preserve spaces", () => {
+    const input = "You are an excellent spy";
+    const alphabet = "xoyqmcgrukswaflnthdjpzibev";
+
+    const actual = substitution(input, alphabet);
+    const expected = 'elp xhm xf mbymwwmfj dne';
+    expect(actual).to.equal(expected);
+
+  });
+
+
+});
