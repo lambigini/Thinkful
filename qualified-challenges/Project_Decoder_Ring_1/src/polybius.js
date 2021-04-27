d8754a2428505d2cfdb0ccf57648bca5d7340d2c
Harrison <harrisontran@outlook.com>
2021-04-26 17:36:47 -0700

    test code for Polybius done

--- qualified-challenges/Project_Decoder_Ring_1/src/polybius.js
+++ qualified-challenges/Project_Decoder_Ring_1/src/polybius.js
@@ -8,6 +8,34 @@ const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
     // your solution code here
+    // encoding 
+    const inputLowerCase = input.toLowerCase();
+    let result = "";
+    let row, col;
+    // loop through each character of the input
+    for (let index = 0; index < inputLowerCase.length; index++) {
+      const character = inputLowerCase[index];
+      console.log(`character: ${character}`)
+
+      // get the row of the table
+       col =parseInt(Math.ceil((character.charCodeAt(0) - 97) /5))+1   ;
+       row = ((character.charCodeAt(0) - 97) % 5) +1;
+
+// if the letter is (i/j) assign number 42 to it
+       if (character === 'i' || character === 'j' ) {
+         col = 2;
+         row = 4;
+       } 
+
+      console.log(`row: ${row}`);
+      console.log(`col: ${col}`)
+      result += (row+ "" + col + " ");
+      
+      console.log(`result: ${result}`)
+    }
+    
+    console.log(`result final: ${result}`)
+    return result;
   }
 
   return {
--- qualified-challenges/Project_Decoder_Ring_1/test/polybius.test.js
+++ qualified-challenges/Project_Decoder_Ring_1/test/polybius.test.js
@@ -1,1 +1,63 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { polybius } = require("../src/polybius");
+
+describe("polybius", () => {
+  it("When encoding, your output should still be a string", () => {
+    const input = "thinkful";
+    const expected = "string";
+    const actual = typeof input;
+    expect(actual).to.equal(expected);
+  });
+
+  it("should encode a message by translating each letter to number pairs", () => {
+    const input = "thinkful";
+    const expected = "44 32 42 33 52 12 54 13";
+    const actual = polybius(input);
+    expect(actual).to.equal(expected);
+  });
+
+  it("should translate both 'i' and 'j' to 42", () => {
+    const input = "ij";
+    const expected = "4242";
+    const actual = polybius(input);
+    expect(actual).to.equal(expected);
+  });
+
+  it("should leave spaces as is", () => {
+    const input = "Hello world";
+    const expected = "3251131343 2543241341";
+    const actual = polybius(input);
+    expect(actual).to.equal(expected);
+  });
+
+  it("should decode a message by translating each pair of numbers into a letter", () => {
+    const input = "4432423352125413";
+    const expected = "thinkful";
+    const actual = polybius(input, false);
+    expect(actual).to.equal(expected);
+  });
+
+
+  it("should translate 42 to both 'i' and 'j'", () => {
+    const input = "42";
+    const expected = "(i/j)";
+    const actual = polybius(input, false);
+    expect(actual).to.equal(expected);
+  });
+
+  it("should leave spaces as is", () => {
+    const input = "3251131343 2543241341";
+    const expected = "hello world";
+    const actual = polybius(input, false);
+    expect(actual).to.equal(expected);
+  });
+
+  it("should return false if the length of all numbers is odd", () => {
+    const input = "44324233521254134";
+    const expected = false
+    const actual = polybius(input, false);
+    expect(actual).to.equal(expected);
+  });
+
+});
