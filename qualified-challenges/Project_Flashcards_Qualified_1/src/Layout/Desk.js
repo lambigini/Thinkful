c4ba25e9d9518136e743905b9cd1545e56715950
Harrison <harrisontran@outlook.com>
2021-06-30 21:59:35 -0700

    project flashcard

---
+++ qualified-challenges/Project_Flashcards_Qualified_1/src/Layout/Desk.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Desk() {
+  return (
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <button type="button" class="btn btn-secondary">
+          View
+        </button>
+        <button type="button" class="btn btn-primary">
+          Study
+        </button>
+        <button type="button" class="btn btn-danger">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Desk;
---
+++ qualified-challenges/Project_Flashcards_Qualified_1/src/Layout/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import NotFound from "./NotFound";
+import { listDecks } from "../utils/api/index";
+import Desks from "./Desks";
+function Layout() {
+  const [listDesks, setListDesks] = useState([]);
+
+  useEffect(() => {
+    async function getListOfDesk() {
+      const listOfDesksFromAPI = await listDecks();
+
+      setListDesks((current) => (current = listOfDesksFromAPI));
+    }
+
+    getListOfDesk();
+    // console.log("listDesks", listDesks);
+  }, []);
+
+  console.log("listDesks", listDesks);
+  return (
+    <>
+      <Header />
+      <div className="container">
+        {/* TODO: Implement the screen starting here */}
+
+        <button onClick={() => console.log("button clicked")}>
+          {" "}
+          Create Desk
+        </button>
+
+        <Desks listDesks={listDesks} />
+
+        {/* <NotFound /> */}
+      </div>
+    </>
+  );
+}
+
+export default Layout;
