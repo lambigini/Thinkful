e97880f85b3dbe3c1ff410d0909c308c1ef4e875
Harrison <harrisontran@outlook.com>
2021-07-14 19:35:26 -0700

    all test pass

--- qualified-challenges/Project_Flashcards_Qualified_1/src/DecksRoute/EditCard.js
+++ qualified-challenges/Project_Flashcards_Qualified_1/src/DecksRoute/EditCard.js
@@ -1,8 +1,125 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { useParams, useRouteMatch, Link, useHistory } from "react-router-dom";
+import { createCard, readCard, readDeck, updateCard } from "../utils/api";
 
 function EditCard() {
-  return <h2> inside EditCard function</h2>;
+  const { deckId, cardId } = useParams();
+  const { url } = useRouteMatch();
+  const history = useHistory();
+  const [updateCurrentCard, setUpdateCurrentCard] = useState({
+    front: "",
+    back: "",
+    deckId: deckId,
+  });
+
+  const [currentDeck, setCurrentDeck] = useState({
+    id: 0,
+    name: "",
+    description: "",
+  });
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    async function getCurrentDeck() {
+      const response = await readDeck(deckId, abortController.signal);
+
+      setCurrentDeck((current) => ({ ...current, ...response }));
+
+      const loadCard = await readCard(cardId, abortController.signal);
+
+      setUpdateCurrentCard((current) => ({ ...current, ...loadCard }));
+    }
+    getCurrentDeck();
+  }, [deckId]);
+
+  const handleChange = ({ target }) => {
+    setUpdateCurrentCard(
+      (current) =>
+        (current = {
+          ...current,
+          [target.name]: target.value,
+        })
+    );
+  };
+
+  console.log("updateCurrentCard,", updateCurrentCard);
+
+  const handleSubmit = (event) => {
+    const abortController = new AbortController();
+    event.preventDefault();
+
+    const newcard = updateCard(updateCurrentCard, abortController.signal);
+
+    console.log("newcard,", newcard);
+
+    setUpdateCurrentCard((current) => ({
+      ...current,
+      ["front"]: " ",
+      ["back"]: " ",
+      ["deckId"]: deckId,
+    }));
+
+    history.push(`/decks/${deckId}`);
+  };
+
+  return (
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <a href="/">Home</a>
+          </li>
+          <li class="breadcrumb-item">
+            <a href={url}>{currentDeck.name}</a>
+          </li>
+          <li className="breadcrumb-item">Edit Card {cardId}</li>
+        </ol>
+      </nav>
+      <h1> Edit Card</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="front">
+          Front:
+          <br></br>
+          <textarea
+            id="front"
+            type="text"
+            name="front"
+            required
+            // placeholder="Front side of the card"
+            autoFocus="on"
+            rows="10"
+            cols="60"
+            onChange={handleChange}
+            value={updateCurrentCard.front}
+          ></textarea>
+        </label>
+        <br></br>
+        <label htmlFor="back">Back: </label>
+        <br></br>
+        <textarea
+          id="back"
+          type="text"
+          name="back"
+          required
+          placeholder="Back of the card"
+          autoFocus="on"
+          rows="10"
+          cols="60"
+          onChange={handleChange}
+          value={updateCurrentCard.back}
+        ></textarea>
+
+        <br></br>
+        <Link to={`/decks/${deckId}`} className="btn btn-secondary">
+          Done
+        </Link>
+        <button type="submit" className="btn btn-primary">
+          Save
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default EditCard;
