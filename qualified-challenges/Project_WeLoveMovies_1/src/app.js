8084a2980f8869615c678ae602a34f780943af4a
Harrison <harrisontran@outlook.com>
2021-10-08 20:46:09 -0700

    add routes, error and not found handler

--- qualified-challenges/Project_WeLoveMovies_1/src/app.js
+++ qualified-challenges/Project_WeLoveMovies_1/src/app.js
@@ -2,4 +2,41 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// Get all movies
+app.use("/movies", moviesRouter);
+app.use("/movies?is_showing=true", moviesRouter);
+
+// Read one movie
+app.use("/movies/:movieId", moviesRouter);
+app.use("/movies/:movieId/theaters", moviesRouter);
+app.use("/movies/:movieId/reviews", moviesRouter);
+
+// Destroy review
+app.use("/reviews/:reviewId", reviewRouter);
+
+// update review
+
+app.use("/reviews/:reviewId", reviewRouter);
+
+// get all theaters
+
+app.use("theaters", theatersRouter);
+
+// not found handler
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: `Not found: ${req.originalUrl}`,
+  });
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong" } = error;
+  res.status(status).json({ error: message });
+});
+
 module.exports = app;
